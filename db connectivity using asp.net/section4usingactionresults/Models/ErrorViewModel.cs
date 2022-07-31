fda9c45389abec91516bad4e5b3e18f26c68fec4
shalini1708 <shalini.k1@mphasis.com>
2022-07-31 22:11:27 +0530

    using action results

---
+++ db connectivity using asp.net/section4usingactionresults/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace section4usingactionresults.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
