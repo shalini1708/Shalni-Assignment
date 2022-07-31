c527ecbc25b984a97ed0b4248386a1474eee43b6
shalini1708 <shalini.k1@mphasis.com>
2022-07-31 21:37:58 +0530

    asp.net using controller

---
+++ db connectivity using asp.net/section4usingcontroller/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using section4usingcontroller.Models;
+
+namespace section4usingcontroller.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Info(String t)
+        {
+            ViewData["Message"] = "You have requested info for " + t;
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult StudentDetails(String t)
+        {
+            StringBuilder sb = new StringBuilder("<b>Student details entered:</b><br>");
+            sb.Append("Name: " + Request.Form["name"].ToString() + "<Br>");
+            sb.Append("Address: " + Request.Form["address"].ToString() + "<Br>");
+            sb.Append("Subject: " + Request.Form["subject"].ToString() + "<Br>");
+
+            ViewData["message"] = sb.ToString();
+            return View();
+        }
+
+    }
+}
+
+
