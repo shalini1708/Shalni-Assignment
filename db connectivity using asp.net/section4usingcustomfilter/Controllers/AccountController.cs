5892ad11ea2f7c727dda58926a2b7872302b44f6
shalini1708 <shalini.k1@mphasis.com>
2022-08-01 08:33:41 +0530

    using custom filter

---
+++ db connectivity using asp.net/section4usingcustomfilter/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using section4usingcustomfilter.Models;
+using System.Security.Claims;
+
+
+namespace section4usingcustomfilter.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
+
+
---
+++ db connectivity using asp.net/section4usingcustomfilter/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace section4usingcustomfilter.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
