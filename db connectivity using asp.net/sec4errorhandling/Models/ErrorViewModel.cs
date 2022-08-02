f84f4b90ab1c5c760756e929b316a983d0824883
shalini1708 <shalini.k1@mphasis.com>
2022-08-02 13:06:39 +0530

    using error handling

---
+++ db connectivity using asp.net/sec4errorhandling/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sec4errorhandling.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
